b44ea42dbe14119bf595f6993bb63f5623b71a5f
kavyak <kavyasreeak7@gmail.com>
2022-09-29 14:29:30 +0530

    commit

--- bin/project6.dart
+++ bin/project6.dart
@@ -1,7 +1,7 @@
-void main() {
-  for(int i = 1; i<=100 ; i++){
-    if(i == 51){
-      break;
-  }
-    print(i);
-}
+// void main() {
+//   for(int i = 1; i<=100 ; i++){
+//     if(i == 51){
+//       break;
+//   }
+//     print(i);
+// }
