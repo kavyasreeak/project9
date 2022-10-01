a58be28f9ea8f040ec65b6eb225ada64d554a0b8
kavyak <kavyasreeak7@gmail.com>
2022-10-01 11:16:10 +0530

    commit

---
+++ bin/MAPPP.dart
@@ -0,0 +1,14 @@
+void main(){
+  Map<String,dynamic> map1 ={'id':1,'name':'shoe','price':400,'rating':4.5};
+  print(map1);
+  map1.forEach((key,value){
+    print(value);
+  });
+
+  Map<int,int>map2= Map();
+  map2[1] = 20;  //here 1 3 4 are keys  & 20 30 40 are values
+  map2[2] = 30;
+  map2[3] = 40;
+  // map2['key'] = 200;
+   print(map2);
+}
